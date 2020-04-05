32b24e55b71c9bef28bbc149b189386de1265770
Soham Bhattacharjee <sohamb.1390@gmail.com>
2020-04-05 11:39:42 +0530

    - Initial Setup

---
+++ Covid19-Updates/Covid19-UpdatesTests/Covid19_UpdatesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Covid19_UpdatesTests.swift
+//  Covid19-UpdatesTests
+//
+//  Created by Soham Bhattacharjee on 05/04/20.
+//  Copyright © 2020 Soham Bhattacharjee. All rights reserved.
+//
+
+import XCTest
+@testable import Covid19_Updates
+
+class Covid19_UpdatesTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
